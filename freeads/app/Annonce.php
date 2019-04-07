b37d3988ab2c4d61e5b5dd99e58a9ccdba888a37
Elisa Vallee <elisavallee@MacBook-Pro-de-Elisa-2.local>
2019-04-07 11:49:55 +0200

    plus d'erreur  view

--- freeads/app/Annonce.php
+++ freeads/app/Annonce.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Annonce extends Model
 {
-    protected $table = 'annonce';
-
     protected $filsAnnonce = [
         'titre', 'description', 'photo', 'prix'
     ];
